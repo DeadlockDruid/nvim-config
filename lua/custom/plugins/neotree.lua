03e777157b5a67f203a13d98f700fee921094370
Divyanshu Rathore <divyanshu03@live.com>
2024-11-24 15:36:39 +0530

    Updates neotree plugin

--- lua/custom/plugins/neotree.lua
+++ lua/custom/plugins/neotree.lua
@@ -5,19 +5,19 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- Recommended for icons
     'MunifTanjim/nui.nvim',
-    '3rd/image.nvim', -- Uncomment this line if you need image preview support
+    '3rd/image.nvim',              -- Uncomment this line if you need image preview support
   },
-  cmd = 'Neotree', -- Lazy load NeoTree on this command
+  cmd = 'Neotree',                 -- Lazy load NeoTree on this command
   init = function()
     -- Disable netrw at the very start
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
   end,
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true }, -- Key mapping to reveal file in tree
-    { '<leader>ee', '<cmd>Neotree toggle<CR>', desc = 'Toggle NeoTree file explorer', silent = true },
-    { '<leader>ef', '<cmd>Neotree focus<CR>', desc = 'Focus NeoTree on current file', silent = true },
-    { '<leader>ec', '<cmd>Neotree close<CR>', desc = 'Close NeoTree file explorer', silent = true },
+    { '\\',         ':Neotree reveal<CR>',     desc = 'NeoTree reveal',                silent = true }, -- Key mapping to reveal file in tree
+    { '<leader>ee', '<cmd>Neotree toggle<CR>', desc = 'Toggle NeoTree file explorer',  silent = true },
+    { '<leader>ef', '<cmd>Neotree focus<CR>',  desc = 'Focus NeoTree on current file', silent = true },
+    { '<leader>ec', '<cmd>Neotree close<CR>',  desc = 'Close NeoTree file explorer',   silent = true },
   },
   opts = {
     filesystem = {
@@ -27,7 +27,6 @@ return {
         },
       },
     },
-    close_if_last_window = false,
     popup_border_style = 'rounded',
     enable_git_status = true,
     enable_diagnostics = true,
@@ -43,12 +42,11 @@ return {
       -- Your NeoTree configuration goes here
       filesystem = {
         filtered_items = {
-          visible = true, -- Shows hidden files
-          hide_dotfiles = false, -- Set this to false to show dotfiles (files starting with .)
+          visible = true,          -- Shows hidden files
+          hide_dotfiles = false,   -- Set this to false to show dotfiles (files starting with .)
           hide_gitignored = false, -- Set this to false if you want to see files ignored by .gitignore
         },
       },
     }
   end,
 }
-
