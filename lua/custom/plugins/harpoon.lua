a1f0f54c3906799bbe959c3a31bdd0a5d88b2355
Divyanshu Rathore <divyanshu03@live.com>
2024-11-29 15:32:20 +0530

    Changes in harpoon configuration and it properly leverages telescope ui

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,79 +1,137 @@
 return {
-  {
-    'ThePrimeagen/harpoon',
-    branch = 'harpoon2',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-telescope/telescope.nvim',
-    },
-    config = function()
-      -- Require the necessary modules
-      local harpoon = require 'harpoon'
-      local telescope = require 'telescope'
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    require('harpoon'):setup()
+  end,
+  keys = function()
+    local harpoon = require 'harpoon'
 
-      -- Set up Harpoon
-      harpoon:setup {}
+    local conf = require('telescope.config').values
 
-      -- Key mapping to add the current file to Harpoon
-      vim.keymap.set('n', '<leader>a', function()
-        harpoon:list():add()
-      end, { desc = 'Add file to Harpoon' })
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
 
-      -- Key mappings using Alt key with numbers
-      vim.keymap.set('n', '<M-1>', function()
-        harpoon:list():select(1)
-      end, { desc = 'Go to Harpoon file 1' })
-      vim.keymap.set('n', '<M-2>', function()
-        harpoon:list():select(2)
-      end, { desc = 'Go to Harpoon file 2' })
-      vim.keymap.set('n', '<M-3>', function()
-        harpoon:list():select(3)
-      end, { desc = 'Go to Harpoon file 3' })
-      vim.keymap.set('n', '<M-4>', function()
-        harpoon:list():select(4)
-      end, { desc = 'Go to Harpoon file 4' })
-
-      -- Key mappings to navigate to previous and next Harpoon files (optional)
-      vim.keymap.set('n', '<C-S-P>', function()
-        harpoon:list():prev()
-      end, { desc = 'Go to previous Harpoon file' })
-      vim.keymap.set('n', '<C-S-N>', function()
-        harpoon:list():next()
-      end, { desc = 'Go to next Harpoon file' })
-
-      -- Basic Telescope configuration for Harpoon
-      local function toggle_telescope()
-        local harpoon_list = harpoon:list()
-        local files = {}
-        for _, item in ipairs(harpoon_list.items) do
-          table.insert(files, item.value)
-        end
-
-        require('telescope.pickers')
-            .new({}, {
-              prompt_title = 'Harpoon Files',
-              finder = require('telescope.finders').new_table {
-                results = files,
-              },
-              sorter = require('telescope.config').values.generic_sorter {},
-              attach_mappings = function(prompt_bufnr, map)
-                local actions = require 'telescope.actions'
-                local action_state = require 'telescope.actions.state'
-                actions.select_default:replace(function()
-                  actions.close(prompt_bufnr)
-                  local selection = action_state.get_selected_entry()
-                  if selection ~= nil then
-                    vim.cmd('edit ' .. vim.fn.fnameescape(selection.value))
-                  end
-                end)
-                return true
-              end,
-            })
-            :find()
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
       end
 
-      -- Key mapping to open Harpoon menu using Telescope
-      vim.keymap.set('n', '<M-e>', toggle_telescope, { desc = 'Open Harpoon with Telescope' })
-    end,
-  },
+      local make_finder = function()
+        local paths = {}
+
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(paths, item.value)
+        end
+
+        return require('telescope.finders').new_table {
+          results = paths,
+        }
+      end
+
+      require('telescope.pickers')
+          .new({}, {
+            prompt_title = 'Harpoon',
+            finder = require('telescope.finders').new_table {
+              results = file_paths,
+            },
+            previewer = false,
+            sorter = conf.generic_sorter {},
+            layout_strategy = 'center',
+            layout_config = {
+              preview_cutoff = 1, -- Preview should always show (unless previewer = false)
+
+              width = function(_, max_columns, _)
+                return math.min(max_columns, 80)
+              end,
+
+              height = function(_, _, max_lines)
+                return math.min(max_lines, 15)
+              end,
+            },
+            borderchars = {
+              prompt = { '─', '│', ' ', '│', '╭', '╮', '│', '│' },
+              results = { '─', '│', '─', '│', '├', '┤', '╯', '╰' },
+              preview = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+            },
+            attach_mappings = function(prompt_buffer_number, map)
+              map('i', '<M-r>', function()
+                local state = require 'telescope.actions.state'
+                local selected_entry = state.get_selected_entry()
+                local current_picker = state.get_current_picker(prompt_buffer_number)
+
+                harpoon:list():remove(selected_entry)
+                current_picker:refresh(make_finder())
+              end)
+
+              return true
+            end,
+          })
+          :find()
+    end
+
+    return {
+      {
+        '<M-e>',
+        function()
+          toggle_telescope(harpoon:list())
+        end,
+        desc = 'Harpoon (Telescope)',
+      },
+      {
+        '<M-ed>',
+        function()
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = 'Harpoon (Default)',
+      },
+
+      {
+        '<leader>a',
+        function()
+          harpoon:list():add()
+        end,
+        desc = 'Harpoon: Add',
+      },
+
+      {
+        '<M-1>',
+        function()
+          harpoon:list():select(1)
+        end,
+      },
+
+      {
+        '<M-2>',
+        function()
+          harpoon:list():select(2)
+        end,
+      },
+
+      {
+        '<M-3>',
+        function()
+          harpoon:list():select(3)
+        end,
+      },
+
+      -- Toggle previous & next buffers stored within Harpoon list
+      {
+        '<M-p>',
+        function()
+          harpoon:list():prev()
+        end,
+        desc = 'Harpoon: Previous',
+      },
+
+      {
+        '<M-n>',
+        function()
+          harpoon:list():next()
+        end,
+        desc = 'Harpoon: Next',
+      },
+    }
+  end,
 }
+
