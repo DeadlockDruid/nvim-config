24a7150a64844fdb0d0da00c000fe3c58c4d1bcd
Divyanshu Rathore <divyanshu03@live.com>
2026-03-13 12:08:08 +0530

    Fixes neotree file focus behavior

--- lua/custom/plugins/neotree.lua
+++ lua/custom/plugins/neotree.lua
@@ -1,15 +1,14 @@
 return {
   'nvim-neo-tree/neo-tree.nvim',
-  version = '*', -- Use the latest version branch for stability and new features
+  version = '*',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- Recommended for icons
+    'nvim-tree/nvim-web-devicons',
     'MunifTanjim/nui.nvim',
-    -- '3rd/image.nvim',              -- Uncomment this line if you need image preview support
   },
-  cmd = 'Neotree', -- Lazy load NeoTree on this command
+  cmd = 'Neotree',
+  event = 'BufEnter',        -- fix #2: load early enough to register autocmds
   init = function()
-    -- Disable netrw at the very start
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
   end,
@@ -59,39 +58,37 @@ return {
     close_if_last_window = true,
     window = {
       width = 36,
-      auto_expand_width = false, -- adapts when you have long filenames
+      auto_expand_width = false,
     },
     default_component_configs = {
       name = {
         use_git_status_colors = true,
         trailing_slash = true,
         highlight_opened_files = true,
-        display_path = 'tail', -- show only folder name
+        display_path = 'tail',
       },
-      icon = { folder_closed = '', folder_open = '' },
+      icon = { folder_closed = '', folder_open = '' },
     },
     filesystem = {
       hijack_netrw_behavior = 'disabled',
-      filesystem = {
-        follow_current_file = {
-          enabled = true,
-          leave_dirs_open = true,
-        },
-        bind_to_cwd = false,
+      follow_current_file = {    -- fix #1: lifted out of the erroneous inner block
+        enabled = true,
+        leave_dirs_open = true,
       },
-      use_libuv_file_watcher = true, -- auto refresh on file changes
+      bind_to_cwd = false,       -- fix #1: same
+      use_libuv_file_watcher = true,
       group_empty_dirs = true,
       window = {
         mappings = {
-          ['\\'] = 'close_window', -- Close NeoTree window with '\'
+          ['\\'] = 'close_window',
           ['r'] = 'rename',
           ['R'] = 'refresh',
         },
       },
       filtered_items = {
-        visible = true, -- Shows hidden files
-        hide_dotfiles = false, -- Set this to false to show dotfiles (files starting with .)
-        hide_gitignored = false, -- Set this to false if you want to see files ignored by .gitignore
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
         hide_by_name = { 'node_modules', 'dist', '.venv' },
       },
     },
@@ -100,9 +97,7 @@ return {
     enable_diagnostics = true,
   },
   config = function(_, opts)
-    require('nvim-web-devicons').setup {
-      default = true, -- globally enable default icons
-    }
+    require('nvim-web-devicons').setup { default = true }
     require('neo-tree').setup(opts)
   end,
 }
